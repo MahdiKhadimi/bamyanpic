9ec1ef392e529908eab1edf48e79b0c31782cdff
mahdi khadimi <mahdikhadimi14@gmail.com>
2022-11-23 09:21:45 +0430

    Create Image Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Image::factory(10)->create();
+
     }
 }
