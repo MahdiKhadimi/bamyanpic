5dd61c38e3d77cd3bc5a9d4690406da80a6ff744
mahdi khadimi <mahdikhadimi14@gmail.com>
2022-11-29 21:07:06 +0430

    refactor image edit

--- resources/views/image/edit.blade.php
+++ resources/views/image/edit.blade.php
@@ -1,22 +1,37 @@
-<x-layout title="Edit image">
+
     
-<x-form action="{{ route('images.update',$image->id) }}" method="PUT">
-  
-    <div style="width:400px">
-        <img src="{{ asset($image->fileUrl()) }}" alt="$image->title">
-    </div>
-    <div>
-        <label for="title">Title</label>
-            <input type="text" id="title" name="title" value="{{ old('title',$image->title) }}">
-            @error('title')
-              <div>{{ $message }}</div>
-            @enderror
-    </div>
-    <div>
-        <button type="submit">Edit</button>
-       
-    </div>
+<x-layout title="Update image">
+    <div class="container-fluid mt-4">
+         <div class="row">
+             <div class="col-md-6 offset-md-3">
+                 <div class="card">
+                     <div class="card-header">Update image</div>
+                     <div class="card-body">
+                        <x-form action="{{ route('images.update',$image->id) }}" method="PUT">
 
-
-</x-form>
-</x-layout>
+                        <div>
+                            <img src="{{ asset($image->fileUrl()) }}" class="img-fluid" alt="$image->title">
+                        </div>
+                             <div class="mb-3">
+                                 <label class="form-label" for="title">Photo Title</label>
+                                 <input type="text" name="title" class="form-control @error('title')is-invalid @enderror" value="{{ old('title',$image->title) }}">
+                                 @error('title')
+                                 <div class="invalid-feedback">
+                                     {{ $message }}}
+                                 </div>
+                                 @enderror
+                             </div>
+                             
+                             <div class="mb-3">
+                                 <button type="submit" class="btn btn-primary">Save Changes</button>
+                                 <a href="{{ route('images.index') }}}" class="btn btn-outline-secondary">Cancel</a>
+                             </div>
+                       </x-form>
+                     </div>
+                 </div>
+ 
+             </div>
+         </div>
+     </div>
+ </x-layout>
+ 
