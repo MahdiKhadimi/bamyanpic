1b8dda0d995447f95b4829660bb194723f40e6c3
mahdi khadimi <mahdikhadimi14@gmail.com>
2022-11-30 09:45:57 +0430

    Update image edit

--- resources/views/image/edit.blade.php
+++ resources/views/image/edit.blade.php
@@ -24,7 +24,7 @@
                              
                              <div class="mb-3">
                                  <button type="submit" class="btn btn-primary">Save Changes</button>
-                                 <a href="{{ route('images.index') }}}" class="btn btn-outline-secondary">Cancel</a>
+                                 <a href="{{ route('images.index') }}" class="btn btn-outline-secondary">Cancel</a>
                              </div>
                        </x-form>
                      </div>
