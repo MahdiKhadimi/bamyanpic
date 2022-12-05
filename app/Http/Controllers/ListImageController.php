c4dee4caabecc7d9b6018b8dd33f74554a99a7bb
mahdi khadimi <mahdikhadimi14@gmail.com>
2022-12-05 09:05:26 +0430

    Create search for list image

--- app/Http/Controllers/ListImageController.php
+++ app/Http/Controllers/ListImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Image;
+use App\Models\Scopes\ImageSearchScope;
 use Illuminate\Http\Request;
 
 class ListImageController extends Controller
@@ -13,6 +14,10 @@ class ListImageController extends Controller
         $images = Image::published()->latest()->paginate(15)->withQueryString();
         
         return view('image-list',compact('images'));
-    
+    }
+
+    public static function booted()
+    {
+        static::addGlobalScope(new ImageSearchScope);
     }
 }
--- resources/views/image-list.blade.php
+++ resources/views/image-list.blade.php
@@ -1,26 +1,36 @@
 <x-layout title="free images">
 
+
     <div class="container-fluid mt-4">
 
         @if ($message = session('message'))
-        <x-alert type='info' dismissible='true'>
-            {{ $component->icon() }}
-            {{ $message }}
-        </x-alert>
+            <x-alert type='info' dismissible='true'>
+                {{ $component->icon() }}
+                {{ $message }}
+            </x-alert>
         @endif
-    <div>
-        All Images
-    </div>
-        <div class="row" data-masonry='{"percentPosition": true }'>
-            
-            @foreach ($images as $image)
-            <div class="col-lg-4 col-md-4 col-sm-6 mb-4">
-                <div class="card">
-                    <a href="{{ $image->link() }}">
-                        <img src="{{asset($image->fileUrl())}}" alt="{{ $image->title }}" class="card-img-top">
-                    </a>
-                </div>
+        <div class="row mb-4">
+            <div class='col-md-6'>
+                All Images
             </div>
+            <div class="col-md-6 text-right">
+                <form class="search-form" action="{{ url('/') }}">
+                    <input type="search" name="q" placeholder="Search images by title" aria-label="Search..."
+                        autocomplete="off">
+                </form>
+            </div>
+        </div>
+
+        <div class="row" data-masonry='{"percentPosition": true }'>
+
+            @foreach ($images as $image)
+                <div class="col-lg-4 col-md-4 col-sm-6 mb-4">
+                    <div class="card">
+                        <a href="{{ $image->link() }}">
+                            <img src="{{ asset($image->fileUrl()) }}" alt="{{ $image->title }}" class="card-img-top">
+                        </a>
+                    </div>
+                </div>
             @endforeach
 
         </div>
